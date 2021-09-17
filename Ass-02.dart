6ee8b3d87d4ea626d83e11983da46747d693f29e
JiaNoor <jianoor053@gmail.com>
2021-09-17 22:32:21 +0500

    second commit

--- Ass-02.dart
+++ Ass-02.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(){
   
 //  Q2:	
@@ -18,4 +20,25 @@ void main(){
   lst_1.retainWhere((e) => ! lst_2.contains(e));
   print(lst_1);
 
+ // Q7:
+  var table = [7,14, 21, 28, 35, 42, 49, 56, 63, 70];
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var seventh = Map.fromIterables(numbers,table);
+  print(seventh);
+
+ // Q8:
+  String password = "a1B4C^7";
+  print("Enter Your Password!");
+  String user = stdin.readLineSync();
+  if(user.isEmpty == true){
+    print("Please enter your password");
+  }
+  else{
+    if(user==password){
+      print("Correct! Password you entered matches the original password..");
+    }
+    else{
+      print("Incorrect Password");
+    }
+  }
 }
